ce7a56da485b05ee0bfdc532154eb66c0c7b2032
matashita <t.matashita@gmail.com>
2022-03-28 07:24:35 +0900

    upload to github

---
+++ src/constants.js
@@ -0,0 +1,17 @@
+// constants.js
+// CONTRACT_ADDRESSに、コントラクトアドレスを保存します。
+const CONTRACT_ADDRESS = '0xa991536Ecb19Fb96f4615eE4201A2A9913D74B1B';
+
+//NFT キャラクター の属性をフォーマットしてオブジェクトとして返します。
+const transformCharacterData = (characterData) => {
+  return {
+    name: characterData.name,
+    imageURI: characterData.imageURI,
+    hp: characterData.hp.toNumber(),
+    maxHp: characterData.maxHp.toNumber(),
+    attackDamage: characterData.attackDamage.toNumber(),
+  };
+};
+
+// 変数を constants.js 以外の場所でも使えるようにします。
+export { CONTRACT_ADDRESS, transformCharacterData };
